69047166266958b974ab99aab3e11a7e442a3873
Naira_Reyes <gab1ana1@gmail.com>
2020-07-16 20:43:26 -0400

    Created all buttons

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React from "react";
+import "./Button.css";
 
 // This is a functional component - just sent up a little differently as an arrow function!
-const Button = (props) => (
-    <button>
-        {props.label}
-    </button>
-)
+const Button = (props) => {
+  //   let classList = [];
+  //   classList.push("override");
+  //   let types = ["primary", "danger", "success", "warning"];
 
-export default Button; 
+  //   if (types.includes(props.type)) {
+  //     classList.push(`button-$(props.type}${props.outline})`);
+  //   }
+
+  //   if (props.large) {
+  //     classList.push("button-large");
+  //   }
+
+  //   if (props.small) {
+  //     classList.push("button-small");
+  //   }
+
+  //   if (props.outline) {
+  //     classList.push(`button-${props.type}-outline`);
+  //   }
+
+  //   return <button className={classList.join(" ")}> {props.label}</button>;
+
+  let classList = "";
+  // classList.push('override');
+  let types = ["primary", "danger", "warning", "success", "default"];
+  let large = ["primary", "danger", "warning", "success", "default"];
+
+  // if(types.includes(props.type)){
+  //     classList.push(`button-${props.type}`)
+  // }
+  // if(props.large){
+  //     classList.push('button.large')
+  // }
+  // if([props])
+  // }
+  // return <button className={'button-${props.type}'}>{props.label}</button>
+
+  if (types.includes(props.type)) {
+    classList += ` button-${props.type}`;
+  }
+
+  //   if (types.includes(props.large)) {
+  //     classList += ` button-large`;
+  //   }
+
+  return <button className={classList}>{props.label}</button>;
+};
+export default Button;
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,7 +1,204 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import {
+  storiesOf
+} from "@storybook/react";
 
-import Button from './Button';
+import Button from "./Button";
 
-storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+storiesOf("Button", module)
+  .add("Primary", () =>
+    <
+    Button label = "Do Something"
+    type = "primary" / >
+  )
+  .add("Hover Primary", () =>
+    <
+    Button label = "Do Something"
+    type = "primary" / >
+  )
+  .add("Outline Primary", () =>
+    <
+    Button label = "Do Something"
+    type = "primary" / >
+  )
+  .add("Filled Primary", () =>
+    <
+    Button label = "Do Something"
+    type = "primary" / >
+  )
+
+  //Large Primary
+  .add("Large Primary", () =>
+    <
+    Button label = "Do Something"
+    type = "primary" / >
+  )
+  .add("Large Filled Primary", () =>
+    <
+    Button label = "Do Something"
+    type = "primary" / >
+  )
+  .add("Large Outline Primary", () =>
+    <
+    Button label = "Do Something"
+    type = "primary" / >
+  )
+
+  //Regular Danger
+
+  .add("Danger", () =>
+    <
+    Button label = "Do Something"
+    type = "danger" / >
+  )
+  .add("Hover Danger", () =>
+    <
+    Button label = "Do Something"
+    type = "danger" / >
+  )
+  .add("Outline Danger", () =>
+    <
+    Button label = "Do Something"
+    type = "Danger" / >
+  )
+  .add("Filled Danger", () =>
+    <
+    Button label = "Do Something"
+    type = "danger" / >
+  )
+
+  //Large Danger
+  .add("Large Danger", () =>
+    <
+    Button label = "Do Something"
+    type = "danger" / >
+  )
+  .add("Large Filled Danger", () =>
+    <
+    Button label = "Do Something"
+    type = "danger" / >
+  )
+  .add("Large Outline Danger", () =>
+    <
+    Button label = "Do Something"
+    type = "danger" / >
+  )
+
+  //Regular Success
+
+  .add("Success", () =>
+    <
+    Button label = "Do Something"
+    type = "success" / >
+  )
+  .add("Hover Success", () =>
+    <
+    Button label = "Do Something"
+    type = "success" / >
+  )
+  .add("Outline Success", () =>
+    <
+    Button label = "Do Something"
+    type = "Success" / >
+  )
+  .add("Filled Success", () =>
+    <
+    Button label = "Do Something"
+    type = "success" / >
+  )
+
+  //Large Success
+  .add("Large Success", () =>
+    <
+    Button label = "Do Something"
+    type = "success" / >
+  )
+  .add("Large Filled Success", () =>
+    <
+    Button label = "Do Something"
+    type = "success" / >
+  )
+  .add("Large Outline Success", () =>
+    <
+    Button label = "Do Something"
+    type = "success" / >
+  )
+  //Regular Warning
+
+  .add("Warning", () =>
+    <
+    Button label = "Do Something"
+    type = "warning" / >
+  )
+  .add("Hover Warning", () =>
+    <
+    Button label = "Do Something"
+    type = "warning" / >
+  )
+  .add("Outline Warning", () =>
+    <
+    Button label = "Do Something"
+    type = "warning" / >
+  )
+  .add("Filled Warning", () =>
+    <
+    Button label = "Do Something"
+    type = "warning" / >
+  )
+
+  //Large Warning
+  .add("Large Warning", () =>
+    <
+    Button label = "Do Something"
+    type = "warning" / >
+  )
+  .add("Large Filled Warning", () =>
+    <
+    Button label = "Do Something"
+    type = "warning" / >
+  )
+  .add("Large Outline Warning", () =>
+    <
+    Button label = "Do Something"
+    type = "warning" / >
+  )
+
+  //Regular Default
+
+  .add("Default", () =>
+    <
+    Button label = "Do Something"
+    type = "default" / >
+  )
+  .add("Hover Default", () =>
+    <
+    Button label = "Do Something"
+    type = "default" / >
+  )
+  .add("Outline Default", () =>
+    <
+    Button label = "Do Something"
+    type = "default" / >
+  )
+  .add("Filled Default", () =>
+    <
+    Button label = "Do Something"
+    type = "default" / >
+  )
+
+  //Large Default
+  .add("Large Default", () =>
+    <
+    Button label = "Do Something"
+    type = "default" / >
+  )
+  .add("Large Filled Default", () =>
+    <
+    Button label = "Do Something"
+    type = "default" / >
+  )
+  .add("Large Outline Default", () =>
+    <
+    Button label = "Do Something"
+    type = "default" / >
+  )
